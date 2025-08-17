9d237895eee50ea27d239dab1d2c246d9840fd93
Carlo Wood <carlo@alinoe.com>
2025-08-17 14:24:31 +0200

    WIP

--- src/version2/Size.h
+++ src/version2/Size.h
@@ -130,8 +130,8 @@ concept RectangleSizeConcept = requires(T rectangle_size) {
 struct Size
 {
  public:
-  static constexpr unsigned int Bx = 10;         // Width in squares of one "king block".
-  static constexpr unsigned int By = 10;         // Height in square of one "king block".
+  static constexpr unsigned int Bx = 5;         // Width in squares of one "king block".
+  static constexpr unsigned int By = 5;         // Height in square of one "king block".
   static constexpr unsigned int Px = 1;
   static constexpr unsigned int Py = 1;
 
